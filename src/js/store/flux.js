588575383fcaecba0f656ab6c94a176329f87bcb
Carlos Banks <carlostbanks@gmail.com>
2022-11-23 23:58:19 +0000

    rendering planets.js

--- src/js/store/flux.js
+++ src/js/store/flux.js
@@ -2,16 +2,25 @@ const getState = ({ getStore, getActions, setStore }) => {
 	return {
 		store: {
 			people: [],
+			planets: [],
 
 
 	
 		},
+
 		actions: {
 			//GET REQUEST FOR PEOPLE
 			getPeople: () => {
 				fetch('https://swapi.dev/api/people/')
 				.then(data => data.json())
 				.then(res => setStore({people: res.results}))
+			},
+
+			//GET REQUEST FOR PLANETS
+			getPlanets: () => {
+				fetch('https://swapi.dev/api/planets/')
+				.then(data => data.json())
+				.then(res => setStore({planets: res.results}))
 			}
 			
 			
--- src/js/views/Planets.js
+++ src/js/views/Planets.js
@@ -1,16 +1,28 @@
 import React from 'react';
-
-
-
-
-
+import { useContext } from 'react';
+import { Context } from '../store/appContext';
 
 const Planets = () => {
 
+    const {store, actions} = useContext(Context);
+    const StarWarsPlanets = store.planets.map((planet, index) => {
+        return (
+            <div key={index} className='container'>
+                <div className='card-header'>
+                    Planet Name: {planet.name}
+                </div>
+                <div className='card-details'>
+                    Planet Diameter: {planet.diameter}
+                    <br></br>
+                    Planet Climate: {planet.climate}
+                </div>
+            </div>
+        )
+    })
 
     return (
         <div>
-            <h1> This is the Planets Card </h1>
+            {StarWarsPlanets}
         </div>
     )
 }
